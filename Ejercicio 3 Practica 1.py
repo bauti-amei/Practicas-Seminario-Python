a4e35236c6df8a111f7dd07d088f7bad67ad75bc
Bautista Dionisio Ameijeiras Cavallin <bautista.ameijeiras@gmail.com>
2025-03-16 11:00:16 -0300

    Agrego ejercicio 3

---
+++ Ejercicio 3 Practica 1.py
@@ -0,0 +1,11 @@
+'''
+Crea un programa que calcule la suma de los primeros 100 números
+naturales utilizando un bucle for.
+
+'''
+
+suma = 0
+for i in range (1,101):
+    suma = suma + i
+
+print (f"La suma es {suma}" )
