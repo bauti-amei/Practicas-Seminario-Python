4359bf6881268181ca8ee6ad59f0efc0807b1beb
Bautista Dionisio Ameijeiras Cavallin <bautista.ameijeiras@gmail.com>
2025-03-19 18:58:11 -0300

    Agrego algunos comentarios

--- Consigna clase 2/Promedio peliculas.py
+++ Consigna clase 2/Promedio peliculas.py
@@ -1,10 +1,13 @@
 
 import funciones
 
+# Cargo lista por teclado con las duraciones de las peliculas
 movies_duration = []
 funciones.cargar_lista(movies_duration)
 
+# Calculo el promedio
 prom = funciones.calcular_promedio(movies_duration)
+# Si la funcion retorna -1, la lista esta vacia
 if prom != -1:
     print(f"El promedio de duracion de las peliculas es {prom}")
     print(
